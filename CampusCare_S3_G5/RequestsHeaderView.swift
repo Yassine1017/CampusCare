a46508ec63c766d8074dae82fbfd90e8a17f3178
Yassine1017 <Yassinebenmansour2002@gmail.com>
2025-12-16 22:04:05 +0300

    Page 1 with full constraints

---
+++ CampusCare_S3_G5/RequestsHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  RequestsHeaderView.swift
+//  CampusCare_S3_G5
+//
+//  Created by Mawlid Benmansour on 16/12/2025.
+//
+
+import UIKit
+
+class RequestsHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
